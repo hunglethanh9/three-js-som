9090c582de553332389e74adf376cc5304462463
Jan Wojtecki <janwojtecki@gmail.com>
2018-08-15 13:11:29 +0200

    add simple dat.gui controls

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-var THREE = require('three')
-var OrbitControls = require('three-orbit-controls')(THREE)
+const THREE = require('three')
+const OrbitControls = require('three-orbit-controls')(THREE)
+const dat = require('dat.gui');
+
+var params = {
+    rotationX: .01,
+    rotationY: .01
+}
+
+const TAU = 2 * Math.PI;
 
 start();
 render();
@@ -10,25 +18,32 @@ function start() {
     aspect = window.innerWidth / window.innerHeight;
     fov = 60;
     camera = new THREE.PerspectiveCamera(fov, aspect);
-    camera.position.set(0, 0, 2);
+    camera.position.set(1, 1, 1);
     camera.lookAt(new THREE.Vector3());
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    controls = new OrbitControls(camera);
+    controls = new OrbitControls(camera, renderer.domElement);
 
     var geometry = new THREE.EdgesGeometry(new THREE.BoxGeometry(1, 1, 1));
     var material = new THREE.LineBasicMaterial({ color: 0xffffff, linewidth: 2 });
     box = new THREE.LineSegments(geometry, material);
     scene.add(box);
+
+    var axesHelper = new THREE.AxesHelper();
+    scene.add(axesHelper);
+
+    gui = new dat.GUI();
+    gui.add(params, 'rotationX', .0, .1);
+    gui.add(params, 'rotationY', .0, .1);
 }
 
 function render() {
     requestAnimationFrame(render);
-    box.rotation.x += .01;
-    box.rotation.y += .01;
+    box.rotation.x += params.rotationX;
+    box.rotation.y += params.rotationY;
     renderer.render(scene, camera);
 }
 
