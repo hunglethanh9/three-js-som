5badceba5f806e0d141d8cb651ae9fad92947ba1
Jan Wojtecki <janwojtecki@gmail.com>
2018-08-15 21:03:59 +0200

    add sphere point sampling

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const OrbitControls = require('three-orbit-controls')(THREE)
 const dat = require('dat.gui');
 
 params = {
-    neuronsInitialSpread: 3,
+    neuronsInitialRadius: 5,
     neuronsAmount: 100,
 }
 
 initThree();
 initNeurons();
-initTarget = () => initSphere();
-initTarget();
+initTarget = (size) => initSphere(size / 2.0);
+initTarget(10);
 render();
 
 function initThree() {
@@ -47,11 +47,8 @@ function render() {
 function initNeurons() {
     var neuronCoords = [];
     for (var i = 0; i < params.neuronsAmount; i++) {
-        neuronCoords.push(
-            params.neuronsInitialSpread * (.5 - Math.random()),
-            params.neuronsInitialSpread * (.5 - Math.random()),
-            params.neuronsInitialSpread * (.5 - Math.random())
-        );
+        var point = sampleSphere(params.neuronsInitialRadius);
+        neuronCoords.push(point.x, point.y, point.z);
     }
     var geometry = new THREE.BufferGeometry();
     var vertices = new Float32Array(neuronCoords);
@@ -73,12 +70,24 @@ function onWindowResize() {
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
-
-function initSphere() {
-    var radius = 5;
+function initSphere(radius) {
     var geometry = new THREE.IcosahedronBufferGeometry(radius, 2);
     var wireframe = new THREE.WireframeGeometry(geometry);
     var material = new THREE.LineBasicMaterial({ color: 0x000000 });
     var lines = new THREE.LineSegments(wireframe, material);
     scene.add(lines);
+}
+
+// http://mathworld.wolfram.com/SpherePointPicking.html
+function sampleSphere(radius) {
+    var u = Math.random();
+    var v = Math.random();
+    var theta = 2 * Math.PI * u;
+    var phi = Math.acos(2 * v - 1);
+    var sinPhi = Math.sin(phi);
+    var adjustedRandomRadius = radius * Math.cbrt(Math.random());
+    var x = adjustedRandomRadius * Math.cos(theta) * sinPhi;
+    var y = adjustedRandomRadius * Math.sin(theta) * sinPhi
+    var z = adjustedRandomRadius * Math.cos(phi);
+    return new THREE.Vector3(x, y, z);
 }
